ed88a1fab4766b7dce76189f7bbcd2c3cf7dd691
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-11-12 08:35:12 +0000

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -8,12 +8,6 @@ locals {
   vpc_cidr                      = var.vpc_cidr
   num_of_azs                    = 2
   azs                           = slice(data.aws_availability_zones.available.names, 0, local.num_of_azs)
-  airflow_name                  = "airflow"
-  airflow_service_account       = "airflow-webserver-sa"
-  airflow_webserver_secret_name = "airflow-webserver-secret-key"
-  efs_storage_class             = "efs-sc"
-  efs_pvc                       = "airflowdags-pvc"
-
 
   tags = {
     Name  = local.name
